6935ffa645dace07da2d44310d13bee68c192684
Jazzy Cordaro <jazzy_cordaro@hotmail.com>
2016-08-27 21:05:03 -0500

    pushing to the DOM

--- script.js
+++ script.js
@@ -34,14 +34,16 @@ var showInfo = function(){
   for (var i = 0; i < information.length; i++) {
   var monthly = calculateIncome(information[i].yearlySalary);
   totalMonthly += monthly;
- addInfo += '<h1>' + information[ i ].firstName + ' ' + information[ i ].lastName + ' ' + information[ i ].idNum + ' ' + monthly +  ' ' + '</h1>';
+  addInfo += '<h1>' + information[ i ].firstName + ' ' + information[ i ].lastName + ' ' + information[ i ].idNum + ' ' + monthly +  ' ' + '</h1>';
 
 }
 
-  console.log( ' totalMonthly', totalMonthly );
+console.log( ' totalMonthly', totalMonthly );
 
 
 document.getElementById( 'addInfo' ).innerHTML = addInfo;
 console.log('in addInfo');
 console.log( information );
+document.getElementById("totalMonthly").innerHTML= totalMonthly;
+
 }
